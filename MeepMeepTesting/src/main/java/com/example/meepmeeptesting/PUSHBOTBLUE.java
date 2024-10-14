ff08a1dcfec3108d1759da7b897d933cfabf88fd
midK-night <pasum462@gmail.com>
2024-10-14 16:10:28 -0500

    update pushbotblue
    fix meepmeep based on pbred

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PUSHBOTBLUE.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PUSHBOTBLUE.java
@@ -17,27 +17,53 @@ public class PUSHBOTBLUE  {
 
         Vector2d basket = new Vector2d(55,55);
         Vector2d spawn2 = new Vector2d(55,15);
-        Vector2d spawn3 = new Vector2d(65,15);
+        Vector2d spawn3 = new Vector2d(62,15);
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, 61, Math.toRadians(-55)))
                 .lineToY(10)
                 .waitSeconds(0.0001)
-                .turn(Math.toRadians(135))
+                .turn(Math.toRadians(145))
                 .waitSeconds(0.0001)
                 .splineTo(basket,Math.toRadians(45))
+                .waitSeconds(0.0001)
                 .lineToX(30)
+                .turn(Math.toRadians(-80))
                 .waitSeconds(0.0001)
-                .splineTo(spawn2,Math.toRadians(90))
+                .lineToX(55)
+                .turn(Math.toRadians(125))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(45))
-                .lineToX(40)
+                .splineTo(basket,Math.toRadians(90))
+                .waitSeconds(0.0001)
+                .splineTo(new Vector2d(42, 10), Math.toRadians(0))
                 .waitSeconds(0.0001)
                 .splineTo(spawn3,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(45))
-                .lineToX(40)
+                .lineToY(52)
                 .waitSeconds(0.0001)
                 .splineTo(new Vector2d(20,15),Math.toRadians(180))
                 .build());
+
+        /*
+        * .lineToY(-10)
+                .waitSeconds(0.0001)
+                .turn(Math.toRadians(145))
+                .waitSeconds(0.0001)
+                .splineTo(basket,Math.toRadians(-135))
+                .waitSeconds(0.0001)
+                .lineToX(-30)
+                .waitSeconds(0.0001)
+                .splineTo(spawn2,Math.toRadians(-90))
+                .waitSeconds(0.0001)
+                .splineTo(basket,Math.toRadians(-90))
+                .waitSeconds(0.0001)
+                .splineTo(new Vector2d(-42, -10), Math.toRadians(180))
+                .waitSeconds(0.0001)
+                .splineTo(spawn3,Math.toRadians(-90))
+                .waitSeconds(0.0001)
+                .lineToY(-52)
+                .waitSeconds(0.0001)
+                .splineTo(new Vector2d(-20,-15),Math.toRadians(0))
+                .build());
+        * */
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
