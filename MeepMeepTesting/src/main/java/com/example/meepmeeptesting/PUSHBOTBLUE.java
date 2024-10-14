a6b13be47ec49b4a8aae28e44608aeed76a7c785
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-10-14 13:51:30 -0500

    PUSHBOT BLUE AND RED REWORK

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PUSHBOTBLUE.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PUSHBOTBLUE.java
@@ -6,7 +6,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class PUSHBOTRED  {
+public class PUSHBOTBLUE  {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
 
@@ -15,28 +15,28 @@ public class PUSHBOTRED  {
                 .setConstraints(100, 100, Math.toRadians(270), Math.toRadians(270), 15)
                 .build();
 
-        Vector2d basket = new Vector2d(-55,-55);
-        Vector2d spawn2 = new Vector2d(-55,-15);
-        Vector2d spawn3 = new Vector2d(-65,-15);
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-10, -61, Math.toRadians(125)))
-                .lineToY(-10)
+        Vector2d basket = new Vector2d(55,55);
+        Vector2d spawn2 = new Vector2d(55,15);
+        Vector2d spawn3 = new Vector2d(65,15);
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, 61, Math.toRadians(-55)))
+                .lineToY(10)
                 .waitSeconds(0.0001)
                 .turn(Math.toRadians(135))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135))
-                .lineToX(-30)
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(30)
                 .waitSeconds(0.0001)
-                .splineTo(spawn2,Math.toRadians(-90))
+                .splineTo(spawn2,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135))
-                .lineToX(-40)
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(40)
                 .waitSeconds(0.0001)
-                .splineTo(spawn3,Math.toRadians(-90))
+                .splineTo(spawn3,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135))
-                .lineToX(-40)
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(40)
                 .waitSeconds(0.0001)
-                .splineTo(new Vector2d(-20,-15),Math.toRadians(0))
+                .splineTo(new Vector2d(20,15),Math.toRadians(180))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_BS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_BS.java
@@ -13,39 +13,39 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.tuningVal.MecanumDrive;
 
 @Config
-@Autonomous(name = "PSUHBOTRS", group = "Autonomous")
-public class PUSHBOT_RS extends LinearOpMode {
+@Autonomous(name = "PSUHBOTBS", group = "Autonomous")
+public class PUSHBOT_BS extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        Pose2d initialPose = new Pose2d(-10, -61, Math.toRadians(125));
+        Pose2d initialPose = new Pose2d(10, 61, Math.toRadians(155));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
-        Vector2d basket = new Vector2d(-55,-55);
-        Vector2d spawn2 = new Vector2d(-55,-15);
-        Vector2d spawn3 = new Vector2d(-65,-15);
+        Vector2d basket = new Vector2d(55,55);
+        Vector2d spawn2 = new Vector2d(55,15);
+        Vector2d spawn3 = new Vector2d(65,15);
         TrajectoryActionBuilder pgm = drive.actionBuilder(initialPose)
-                .lineToY(-10)
+                .lineToY(10)
                 .waitSeconds(0.0001)
                 .turn(Math.toRadians(135))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135))
-                .lineToX(-30)
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(30)
                 .waitSeconds(0.0001)
-                .splineTo(spawn2,Math.toRadians(-90))
+                .splineTo(spawn2,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135))
-                .lineToX(-40)
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(40)
                 .waitSeconds(0.0001)
-                .splineTo(spawn3,Math.toRadians(-90))
+                .splineTo(spawn3,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .lineToX(-40)
-                .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(-135));
+                .splineTo(basket,Math.toRadians(45))
+                .lineToX(40);
+
 
 
         Action trajectoryActionCloseOut = pgm.fresh()
-                .splineTo(new Vector2d(-15,-15),Math.toRadians(0))
+                .splineTo(new Vector2d(20,15),Math.toRadians(180))
                 .build();
 
 
