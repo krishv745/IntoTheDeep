5b21fdb02f28a155de99cc749a6746925bee95b0
Riteshvv <vasudevariteshv@gmail.com>
2024-10-13 23:26:52 -0500

    Changed to basic MotorTesting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/MotorTest.java
@@ -1,71 +1,21 @@
 package org.firstinspires.ftc.teamcode.testingFiles;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
     public class MotorTest extends LinearOpMode {
-
-        static final double INCREMENT   = 0.01;     // amount to ramp motor each CYCLE_MS cycle
-        static final int    CYCLE_MS    =   50;     // period of each cycle
-        static final double MAX_FWD     =  1.0;     // Maximum FWD power applied to motor
-        static final double MAX_REV     = -1.0;     // Maximum REV power applied to motor
-
-        // Define class members
-        DcMotor motor;
-        double  power   = 0;
-        boolean rampUp  = true;
-
-
+        DcMotorEx motor;
         @Override
-        public void runOpMode() {
+        public void runOpMode() throws InterruptedException {
+            motor = hardwareMap.get(DcMotorEx.class, "arm");
 
-            // Connect to motor (Assume standard left wheel)
-            // Change the text in quotes to match any motor name on your robot.
-            motor = hardwareMap.get(DcMotor.class, "left_drive");
-            // Wait for the start button
-            telemetry.addData(">", "Press Start to run Motors." );
-            telemetry.update();
+            // use braking to slow the motor down faster
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             waitForStart();
-
-            // Ramp motor speeds till stop pressed.
-            while(opModeIsActive()) {
-
-                // Ramp the motors, according to the rampUp variable.
-                if (rampUp) {
-                    // Keep stepping up until we hit the max value.
-                    power += INCREMENT ;
-                    if (power >= MAX_FWD ) {
-                        power = MAX_FWD;
-                        rampUp = !rampUp;   // Switch ramp direction
-                    }
-                }
-                else {
-                    // Keep stepping down until we hit the min value.
-                    power -= INCREMENT ;
-                    if (power <= MAX_REV ) {
-                        power = MAX_REV;
-                        rampUp = !rampUp;  // Switch ramp direction
-                    }
-                }
-
-                // Display the current value
-                telemetry.addData("Motor Power", "%5.2f", power);
-                telemetry.addData(">", "Press Stop to end test." );
-                telemetry.update();
-
-                // Set the motor to the new power and pause;
-                motor.setPower(power);
-                sleep(CYCLE_MS);
-                idle();
+            while (opModeIsActive()) {
+                motor.setPower(0.5);
             }
-
-            // Turn off motor and signal done;
-            motor.setPower(0);
-            telemetry.addData(">", "Done");
-            telemetry.update();
-
         }
     }
 
