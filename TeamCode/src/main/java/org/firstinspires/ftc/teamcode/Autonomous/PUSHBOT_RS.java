96d3d18b0b9b86dbef7ca62e6ad1cceb90f5f6b9
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-10-14 10:12:12 -0500

    NAME
    small name cchange

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_RS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_RS.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -11,18 +9,12 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.tuningVal.MecanumDrive;
 
 @Config
 @Autonomous(name = "DO_NOT_RUN", group = "Autonomous")
-public class PUSHBOT_BS extends LinearOpMode {
+public class PUSHBOT_RS extends LinearOpMode {
 //    public class Lift {
 //        private DcMotorEx lift;
 //
