d8de9fcd96995395511f956f616a051200efe2c6
midK-night <pasum462@gmail.com>
2024-10-14 16:24:10 -0500

    update pushbotblue auton
    updated path, vector, and auton code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_BS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PUSHBOT_BS.java
@@ -18,46 +18,45 @@ public class PUSHBOT_BS extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        Pose2d initialPose = new Pose2d(10, 61, Math.toRadians(155));
+        Pose2d initialPose = new Pose2d(10, 61, Math.toRadians(-55));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         Vector2d basket = new Vector2d(55,55);
         Vector2d spawn2 = new Vector2d(55,15);
-        Vector2d spawn3 = new Vector2d(65,15);
-        TrajectoryActionBuilder pgm = drive.actionBuilder(initialPose)
+        Vector2d spawn3 = new Vector2d(62,15);
+        TrajectoryActionBuilder pbb = drive.actionBuilder(initialPose)
                 .lineToY(10)
                 .waitSeconds(0.0001)
-                .turn(Math.toRadians(135))
+                .turn(Math.toRadians(145))
                 .waitSeconds(0.0001)
                 .splineTo(basket,Math.toRadians(45))
+                .waitSeconds(0.0001)
                 .lineToX(30)
+                .turn(Math.toRadians(-80))
                 .waitSeconds(0.0001)
-                .splineTo(spawn2,Math.toRadians(90))
+                .lineToX(55)
+                .turn(Math.toRadians(125))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(45))
-                .lineToX(40)
+                .splineTo(basket,Math.toRadians(90))
+                .waitSeconds(0.0001)
+                .splineTo(new Vector2d(42, 10), Math.toRadians(0))
                 .waitSeconds(0.0001)
                 .splineTo(spawn3,Math.toRadians(90))
                 .waitSeconds(0.0001)
-                .splineTo(basket,Math.toRadians(45))
+                .lineToY(52)
+                .waitSeconds(0.0001)
+                .splineTo(new Vector2d(20,15),Math.toRadians(180))
                 .lineToX(40);
 
-
-
-        Action trajectoryActionCloseOut = pgm.fresh()
-                .splineTo(new Vector2d(20,15),Math.toRadians(180))
-                .build();
-
+        waitForStart();
 
         if (isStopRequested()) return;
 
-        Action trajectoryActionChosen;
-        trajectoryActionChosen = pgm.build();
+        Action trajRunner = pbb.build();
 
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen,
-                        trajectoryActionCloseOut
+                        trajRunner
                 )
         );
     }
