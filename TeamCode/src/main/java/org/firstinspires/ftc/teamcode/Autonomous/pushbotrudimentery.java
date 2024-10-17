4cc07da0c9b7ad4a5d1337a9efb00833a0888597
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-10-16 20:36:25 -0500

    Created rudimentery pushbot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/pushbotrudimentery.java
@@ -0,0 +1,144 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+@Autonomous(name = "lol", group = "Autonomous")
+
+
+
+public class pushbotrudimentery extends LinearOpMode {
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+
+
+    @Override
+    public void runOpMode() {
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+
+
+        // Put initialization blocks here
+        waitForStart();
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+//        frontRightDrive.setPower(0.3);
+//        backLeftDrive.setPower(0.3);
+//        backRightDrive.setPower(-0.3);
+//        frontLeftDrive.setPower(-0.3);
+//        sleep(0.2700);
+//        frontRightDrive.setPower(0);
+//        backLeftDrive.setPower(0);
+//        backRightDrive.setPower(0);
+//        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(-0.3);
+        frontLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(0.3);
+        sleep(400);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        backRightDrive.setPower(0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(-0.3);
+        frontLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(0.3);
+        sleep(1700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        backRightDrive.setPower(0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(-0.3);
+        backLeftDrive.setPower(-0.3);
+        frontLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(-0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(-0.3);
+        backLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        sleep(800);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        backRightDrive.setPower(0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(-0.3);
+        backLeftDrive.setPower(-0.3);
+        frontLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(-0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(-0.3);
+        backLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        sleep(800);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0.3);
+        backLeftDrive.setPower(0.3);
+        frontLeftDrive.setPower(0.3);
+        backRightDrive.setPower(0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(-0.3);
+        backLeftDrive.setPower(-0.3);
+        frontLeftDrive.setPower(-0.3);
+        backRightDrive.setPower(-0.3);
+        sleep(2700);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+    }
+
+}
+
