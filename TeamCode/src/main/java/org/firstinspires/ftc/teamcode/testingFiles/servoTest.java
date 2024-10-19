fbf7e6ee3b4450dfad993e64d483d571a104fdf4
Riteshvv <vasudevariteshv@gmail.com>
2024-10-18 19:06:23 -0500

    Added a 2 servo testing file
    Krish gave me permission to do this in master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/servoTest.java
@@ -11,8 +11,10 @@ public class servoTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         ElapsedTime timer = new ElapsedTime();
-        Servo testing = hardwareMap.servo.get("servo");
+        Servo servo1 = hardwareMap.servo.get("servo1");
+        Servo servo2 = hardwareMap.servo.get("servo2");
         double val = 0.1;
+        double val2 = 0.1;
         waitForStart();
         if(isStopRequested()) return;
         while (opModeIsActive()) {
@@ -29,10 +31,27 @@ public class servoTest extends LinearOpMode {
                 timer.reset();
             }
             if (val > 0.06 && val < 0.94) {
-                testing.setPosition(val);
+                servo1.setPosition(val);
+
+            }
+            if (timer.milliseconds() > 250) {
+                if (gamepad1.a) {
+                    val2 += 0.1;
+                } else if (gamepad1.b) {
+                    val2 += 0.05;
+                } else if (gamepad1.x) {
+                    val2 -= 0.1;
+                } else if (gamepad1.y) {
+                    val2 -= 0.05;
+                }
+                timer.reset();
+            }
+            if (val2 > 0.06 && val2 < 0.94) {
+                servo1.setPosition(val);
 
             }
             telemetry.addData("pos = ", val);
+            telemetry.addData("pos2 = ", val2);
             telemetry.update();
         }
     }
