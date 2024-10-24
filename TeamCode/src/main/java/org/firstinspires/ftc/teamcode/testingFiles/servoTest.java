d1846373cc4366e3032d066d8a842a23c50ad218
Riteshvv <vasudevariteshv@gmail.com>
2024-10-23 19:03:00 -0500

    Updated servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/servoTest.java
@@ -1,20 +1,17 @@
+
 package org.firstinspires.ftc.teamcode.testingFiles;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-
 @TeleOp(group = "testing")
-public class servoTest extends LinearOpMode {
+public class servotest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         ElapsedTime timer = new ElapsedTime();
-        Servo servo1 = hardwareMap.servo.get("servo1");
-        Servo servo2 = hardwareMap.servo.get("servo2");
+        Servo testing = hardwareMap.servo.get("servo");
         double val = 0.1;
-        double val2 = 0.1;
         waitForStart();
         if(isStopRequested()) return;
         while (opModeIsActive()) {
@@ -31,27 +28,10 @@ public class servoTest extends LinearOpMode {
                 timer.reset();
             }
             if (val > 0.06 && val < 0.94) {
-                servo1.setPosition(val);
+                testing.setPosition(val);
 
             }
-            if (timer.milliseconds() > 250) {
-                if (gamepad1.a) {
-                    val2 += 0.1;
-                } else if (gamepad1.b) {
-                    val2 += 0.05;
-                } else if (gamepad1.x) {
-                    val2 -= 0.1;
-                } else if (gamepad1.y) {
-                    val2 -= 0.05;
-                }
-                timer.reset();
-            }
-            if (val2 > 0.06 && val2 < 0.94) {
-                servo1.setPosition(val);
-
-            }
-            telemetry.addData("pos = ", val);
-            telemetry.addData("pos2 = ", val2);
+            telemetry.addData("pos = ", testing.getPosition());
             telemetry.update();
         }
     }
